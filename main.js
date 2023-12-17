418250526c1095211690b7511c92dfaab2ba2756
Miro29147 <miro29147@gmail.com>
2023-12-18 01:16:02 +0200

    fix/empty-string-edge-case

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-const text = document.getElementById('area');
-const counter = document.getElementById("counter")
-const text1 = document.getElementById('area-1');
-const counter1 = document.getElementById("counter-1")
+const text = document.getElementById("area");
+const counter = document.getElementById("counter");
+const text1 = document.getElementById("area-1");
+const counter1 = document.getElementById("counter-1");
 
-counter.onmousedown = function(e){
+counter.onmousedown = function (e) {
   e.preventDefault();
-}
-
+};
 
 // FIRST ATTEMPT
-text.addEventListener('input', function handleChange(event) {
-  currentText = event.target.innerText.replace(/<\/span>/g,'').replace(/<span>/g, '')
-  counter.value = getWordCount(currentText)
-  highlightWord(getLongestWord(currentText), currentText)
+text.addEventListener("input", function handleChange(event) {
+  currentText = event.target.innerText
+    .replace(/<\/span>/g, "")
+    .replace(/<span>/g, "");
+  counter.value = getWordCount(currentText);
+  highlightWord(getLongestWord(currentText), currentText);
 });
 
 function getWordCount(text) {
@@ -22,31 +23,38 @@ function getWordCount(text) {
 const getLongestWord = (text) => {
   let longestWord = "";
   const stringArray = text.trim().split(/\s+/g);
-  stringArray.forEach(string => {
-      string.split(" ").forEach(word => {
-          longestWord = (word.length > longestWord.length) ? word : longestWord 
-      })
-  })
-  return longestWord
-}
+  stringArray.forEach((string) => {
+    string.split(" ").forEach((word) => {
+      longestWord = word.length > longestWord.length ? word : longestWord;
+    });
+  });
+  return longestWord;
+};
 
-function highlightWord(longestWord, currentText){
-  text.innerHTML = currentText.replace(longestWord, `<span>${longestWord}</span>`);
+function highlightWord(longestWord, currentText) {
+  text.innerHTML = currentText.replace(
+    longestWord,
+    `<span>${longestWord}</span>`
+  );
 }
 
 //SECOND ATTEMPT
-function count(){
-  console.log(text1)
-  const currentText = text1.innerText;
-  counter1.value = getWordCount(currentText);
-  highlightWord1(getLongestWord(currentText), currentText)
-
+function count() {
+  const currentText = text1.innerText.trim();
+  if (currentText !== "") {
+    counter1.value = getWordCount(currentText);
+    highlightWord1(getLongestWord(currentText), currentText);
+  }
 }
 
-function highlightWord1(longestWord, currentText){
+function highlightWord1(longestWord, currentText) {
   const stringArray = currentText.trim().split(/\s+/g);
-  text1.innerHTML = currentText.replace(longestWord, `<span>${longestWord}</span>`);
-  if(longestWord === stringArray[stringArray.length-1]){
-    text1.innerHTML+="&nbsp;";
-  }
+  console.log(stringArray);
+  text1.innerHTML = currentText.replace(
+    longestWord,
+    `<span>${longestWord}</span>`
+  );
+  // if(longestWord === stringArray[stringArray.length-1]){
+  text1.innerHTML += "&nbsp;";
+  // }
 }
